aa18ca59c9eca5a9807a2c3f628617172811fd37
mralimov <mralimov89@gmail.com>
2021-02-05 11:58:40 -0500

    Refactoring PreView and bookmarks

--- src/js/views/bookmarksView.js
+++ src/js/views/bookmarksView.js
@@ -1,4 +1,5 @@
 import icons from 'url:../../img/icons.svg';
+import previewView from './previewView';
 import View from './View.js';
 
 class BookmarksView extends View {
@@ -7,29 +8,10 @@ class BookmarksView extends View {
   _successMessage = '';
 
   _generateMarkup() {
-    return this._data.map(this._generateMarkupPreview).join('');
     console.log(this._data);
-  }
-
-  _generateMarkupPreview(results) {
-    const id = window.location.hash.slice(1);
-
-    return `
-    <li class="preview">
-          <a class="preview__link ${
-            results.id === id ? 'preview__link--active' : ''
-          }" href="${results.id}">
-            <figure class="preview__fig">
-              <img src="${results.image}" alt="${results.title}" />
-            </figure>
-            <div class="preview__data">
-              <h4 class="preview__title">${results.title}</h4>
-              <p class="preview__publisher">${results.publisher}</p>
-              
-            </div>
-          </a>
-        </li>
-    `;
+    return this._data
+      .map((bookmark) => previewView.render(bookmark, false))
+      .join('');
   }
 }
 
--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -1,4 +1,5 @@
 import icons from 'url:../../img/icons.svg';
+import previewView from './previewView';
 import View from './View.js';
 
 class ResultsView extends View {
@@ -11,25 +12,11 @@ class ResultsView extends View {
     console.log(this._data);
   }
 
-  _generateMarkupPreview(results) {
-    const id = window.location.hash.slice(1);
-
-    return `
-    <li class="preview">
-          <a class="preview__link ${
-            results.id === id ? 'preview__link--active' : ''
-          }" href="${results.id}">
-            <figure class="preview__fig">
-              <img src="${results.image}" alt="${results.title}" />
-            </figure>
-            <div class="preview__data">
-              <h4 class="preview__title">${results.title}</h4>
-              <p class="preview__publisher">${results.publisher}</p>
-              
-            </div>
-          </a>
-        </li>
-    `;
+  _generateMarkup() {
+    console.log(this._data);
+    return this._data
+      .map((result) => previewView.render(result, false))
+      .join('');
   }
 }
 
