a310da9250863699afac891ddf37d1ab6eaded73
mralimov <mralimov89@gmail.com>
2021-02-04 22:21:20 -0500

    highlightinh current food recipe

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -17,9 +17,6 @@ export default class View {
   }
 
   update(data) {
-    if (!data || (Array.isArray(data) && data.length === 0))
-      return this.renderError();
-
     this._data = data;
     const newMarkup = this._generateMarkup();
 
@@ -32,13 +29,19 @@ export default class View {
       const curEl = curElements[i];
       console.log(curEl, newEl.isEqualNode(curEl));
 
+      //updates changed tex
       if (
         !newEl.isEqualNode(curEl) &&
-        newEl.firstChild.nodeValue.trim() !== ''
+        newEl.firstChild?.nodeValue.trim() !== ''
       ) {
         curEl.textContent = newEl.textContent;
       }
     });
+    //updates changed attributes
+    if (!newEl.isEqualNode(curEl))
+      Array.from(newEl.attributes).forEach((attr) =>
+        curEl.setAttribute(attr.name, attr.value)
+      );
   }
 
   _clear() {
