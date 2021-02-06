b3484376e844c33495843f3955e0db70e4f78518
mralimov <mralimov89@gmail.com>
2021-02-06 09:49:43 -0500

    Refactoring upload new Recipe

--- src/js/controller.js
+++ src/js/controller.js
@@ -109,6 +109,12 @@ const controlAddRecipe = async function (newRecipe) {
     //Success message
     addRecipeView.renderMessage();
 
+    //Render bookmarkView
+    bookmarksView.render(model.state.bookmarks);
+
+    //change ID in URL
+    window.history.pushState(null, '', `#${model.state.recipe.id}`);
+
     //Close form window
     setTimeout(function () {
       addRecipeView.toggleWindow();
